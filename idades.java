3ad5e31fa6fd8da83d218bdac9fb42c56e1d1190
RenanACS <renanacsoares@gmail.com>
2025-08-20 16:30:10 -0300

    Add files via upload

---
+++ idades.java
@@ -0,0 +1,16 @@
+public class idades {
+    public static void main(String[] args) {
+        int[] idades={12, 25, 17, 30, 15, 19, 40, 8, 22, 16};
+
+        System.out.println("idades maiores que 18 anos");
+
+        for (int idade : idades){
+if (idade > 18) {
+
+    System.out.println(idade);
+}
+
+        }
+    }
+    
+}
